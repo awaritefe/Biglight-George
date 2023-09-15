47278743149768951d2a73e9ab4f6351ec4cae46
Daniel <awaritefe@hotmail.co.uk>
2023-09-15 22:02:11 +0100

    index.js setup for dev environment

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { google } = require("googleapis");
+const app = express(); // run the function to create a express app
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
