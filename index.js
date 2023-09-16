342d68202a83c91c0842ee2d50e4b4f45712cff2
Daniel <awaritefe@hotmail.co.uk>
2023-09-16 14:29:51 +0100

    built rough components to get content to pull through correct places dynamically

--- index.js
+++ index.js
@@ -99,6 +99,47 @@ app.get("/portfolio", async (req, res) => {
   res.render("portfolio", { portfolioData: portfolio.data.values });
 });
 
+app.get("/about", async (req, res) => {
+  const auth = new google.auth.GoogleAuth({
+    keyFile: "credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
+  });
+
+  // Create client instance for auth
+  const client = await auth.getClient();
+
+  // Instance of Google sheets API
+  const googleSheets = google.sheets({ version: "v4", auth: client });
+
+  const spreadsheetId = "1upiscciyDs2WiD0ed7YHx57ckJ8ALvuaC4krTcI2pkE";
+
+  // Get metaData about spreedsheet
+  const metaData = await googleSheets.spreadsheets.get({
+    auth,
+    spreadsheetId,
+  });
+
+  // Read row from Spreadsheet
+  const homepage = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Homepage",
+  });
+  const portfolio = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "Portfolio Page",
+  });
+  const about = await googleSheets.spreadsheets.values.get({
+    auth,
+    spreadsheetId,
+    range: "About Page",
+  });
+
+  // Render the portfolio template with the Google Sheets data
+  res.render("about", { aboutData: about.data.values });
+});
+
 app.listen(port, (req, res) => {
   console.log(`Example app listening on port ${port}`);
 });
